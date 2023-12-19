e9e936bcc7cffaab5b791dccf9fd010772003772
Ketan Nakum <ketan.nakum77@gmail.com>
2023-12-19 18:13:08 +0530

    Add Second activity and put progressbar with button. Base on button click progressbar display hide and show. Add Calling activity multiple ways in kotlin

--- app/src/main/java/com/gurukrupa/databinding/MainActivity.kt
+++ app/src/main/java/com/gurukrupa/databinding/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.gurukrupa.databinding
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.gurukrupa.databinding.databinding.ActivityMainBinding
 
@@ -17,10 +19,32 @@ class MainActivity : AppCompatActivity() {
         binding.buttonSubmit.setOnClickListener {
             displayGreeting()
         }
+
+        binding.buttonStart.setOnClickListener {
+            gotoNextScreen()
+        }
     }
 
     private fun displayGreeting() {
         binding.textGreeting.text = "Hello! " + binding.editTextName.text
         binding.editTextName.setText("")
+    }
+
+    private fun gotoNextScreen(){
+        //Calling second activity 1 way
+        startActivity(Intent(this@MainActivity, ShowHideProgressBarActivity::class.java))
+
+        //Calling second activity 2 way
+        //startActivity<ShowHideProgressBarActivity>(this)
+
+        //Calling second activity 3 way
+        //val intent = Intent(this, ShowHideProgressBarActivity::class.java)
+        //intent.putExtra("key", value)
+        //startActivity(intent)
+
+    }
+
+    private inline fun <reified T> startActivity(context: Context) {
+        startActivity(Intent(context, T::class.java))
     }
 }
---
+++ app/src/main/java/com/gurukrupa/databinding/ShowHideProgressBarActivity.kt
@@ -0,0 +1,32 @@
+package com.gurukrupa.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.gurukrupa.databinding.databinding.ActivityShowHideProgressBarBinding
+
+class ShowHideProgressBarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityShowHideProgressBarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_show_hide_progress_bar)
+        binding.buttonStart.setOnClickListener {
+            hideShowProgressBar()
+        }
+    }
+
+    private fun hideShowProgressBar() {
+        binding.apply {
+            if (progressBar.visibility == View.GONE)
+            {
+                progressBar.visibility = View.VISIBLE
+                buttonStart.text = getString(R.string.stop)
+            }else{
+                progressBar.visibility = View.GONE
+                buttonStart.text = getString(R.string.start)
+            }
+        }
+    }
+}
